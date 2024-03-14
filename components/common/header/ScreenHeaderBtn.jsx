fe6270b83e2d6c26c03c4a3ba44825270577494d
vedantkesharia <keshariavedant@gmail.com>
2024-03-14 21:02:43 +0530

    initial commit

---
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -0,0 +1,17 @@
+import { Image, Pressable } from "react-native";
+
+import styles from "./screenheader.style";
+
+const ScreenHeaderBtn = ({ iconUrl, dimension, handlePress }) => {
+  return (
+    <Pressable style={styles.btnContainer} onPress={handlePress}>
+      <Image
+        source={iconUrl}
+        resizeMode='cover'
+        style={styles.btnImg(dimension)}
+      />
+    </Pressable>
+  );
+};
+
+export default ScreenHeaderBtn;
---
+++ navigation/navigation.js
@@ -0,0 +1,12 @@
+// // navigation.js
+
+// import { createRouter, Stack } from 'expo-router';
+// import Home from './screens/Home';
+// import JobDetails from './screens/JobDetails';
+
+// const Router = createRouter(() => ({
+//   Home: () => <Home />,
+//   JobDetails: () => <JobDetails />,
+// }));
+
+// export default Router;
